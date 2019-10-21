cf6aceea4a3d91e176961149a1c3235a2c8bc24d
BinarySoftware <mkmikolajek@icloud.com>
2019-10-21 20:47:17 +0200

    Schemat Hornera

---
+++ schemat_hornera.scala
@@ -0,0 +1,34 @@
+/**
+  * [[Schemat_Hornera(A,x)]]
+  *
+  * @param A - Lista współczynnikow wielomianu, od
+  *            elementu przy najwyzszej potedze
+  * @param x - niewiadoma dla ktorej szukane jest
+  *            rozwiązanie schematem
+  * @return  - wynik dla wielomianu o współczynnikach A i
+  *            niewiadomej x obliczony schematem hornera
+  *
+  * Aby obliczyć wartość NBS w 10 nalezy jako A podać kolejne cyfry
+  * NBS od cyfry o najwiekszej wadze, a jako x podać 2
+  *
+  * Analogicznie mozna postepowac dla kazdego innego systemu
+  * pozycyjnego
+  *
+  * Podana funkcja wykozystuje specyficzne funkcje dla jezyka
+  * scala do operowania na tablicach, tj. Tail - zwraca tablice
+  * bez 1. elementu, oraz head - 1. element tablicy.
+  *
+  * oznaczenie @tailrec inforrmuje kompilator iz ma do czynienia z
+  * rekurencją ogonową, w celu zażegnania problemu z JVM i nadmiarem
+  * na stosie ( stack overflow )
+  *
+  */
+ object Main {
+   @tailrec def schemat_hornera(A:List[Double], x:Double): Double = {
+     var s : Double = 0.0
+     if(A.length > 1) {
+       s = Schemat_Hornera(A.tail, x)
+     }
+     s*x + A.head
+   }
+ }
