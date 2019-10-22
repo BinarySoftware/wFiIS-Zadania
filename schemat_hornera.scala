2daa8a313f4e7caa458526d521d17a69e0c60212
BinarySoftware <mkmikolajek@icloud.com>
2019-10-22 20:27:35 +0200

    update

--- schemat_hornera.scala
+++ schemat_hornera.scala
@@ -18,9 +18,12 @@ import scala.collection.immutable._
   *
   * Podana funkcja wykozystuje specyficzne funkcje dla jezyka
   * scala do operowania na tablicach, tj. Tail - zwraca tablice
-  * bez 1. elementu, oraz head - 1. element tablicy, lecz od prawej
-  * strony, więc przed uruchomieniem funkcji odwracam kolejność elementów
-  * w liście
+  * bez 1. elementu, oraz head - 1. element tablicy.
+  * 
+  * Z powodu wywoływania rekurencyjnego funkcji, dochodzi do rekurencyjnego
+  * trawersowania tablicy, co powoduje że elementy na jej początku zostaną
+  * użyte do obliczeń dopiero na końcu. Aby temu zapobiec funkcja automatycznie
+  * odwraca tablicę.
   */
  object Main {
    def nonEmpty[T](A:List[T]): Boolean = {
@@ -41,7 +44,7 @@ import scala.collection.immutable._
      }
 
      if(nonEmpty[T](A)){
-      Some(inner(A,x))
+      Some(inner(A.reverse,x))
      } else { 
       None 
      }
@@ -54,14 +57,14 @@ import scala.collection.immutable._
      val t : List[Double] = List(2.0,3.0,5.0,1.5)
      val x : Double = 3.5
      /* Potrzeba odwrócić listę dla zamierzonego działania head */
-     val s : Option[Double] = schemat_hornera(t.reverse,x)
+     val s : Option[Double] = schemat_hornera(t,x)
      println("2x^3 + 3x^2 + 5x + 1.5 dla x = 3.5 to : " + s)
 
      /*
       * Test 2 - liczba binarna 101010 - 42
       */
      val bin : List[Int] = List(1,0,1,0,1,0)
-     val dec : Option[Double] = schemat_hornera(bin.reverse,2)
+     val dec : Option[Double] = schemat_hornera(bin,2)
      println("101010 : " + dec)
 
      /*
