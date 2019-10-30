33f97d4ee03b12edf49380a1aaea7b38a22c28fe
BinarySoftware <mkmikolajek@icloud.com>
2019-10-30 09:50:54 +0100

    add type annotations

--- pseudokod/z5.scala
+++ pseudokod/z5.scala
@@ -1,23 +1,25 @@
 
 Dodaj2Binarne(A,B):[Int] // Dwie tablice zawierające l. binarnie zapisane
-  C <- [n + 1] // C jest puste o rozmiarze n+1
-  for i <- 1 to n
+  C <- [n] // C jest puste o rozmiarze n+1
+  for i <- 0 to n-1
     do sum <- A[i] + B[i] + C[i]
     C[i] <- sum % 2
     C[i + 1] <- sum / 2 //wstawia 1 o 1  w lewo gdy wynik to 10 lub 11
   return  C
-
+  
 Dodaj2U1(A,B) : ([Int],Bool) //Tablice o dlugosci n, zwraca tuple zawierającąj tablice Int oraz bool gdy wystapil nadmiar/niedomiar
-  c <- Dodaj2Binarne(A,B)
-  if length[d] > length[A]
+  C <- Dodaj2Binarne(A,B)
+  if length[C] > length[A]
     then 
-      d <- [n]
-      d[n] <- 1 // pusta tablica o 1 na koncu
-      c <- Dodaj2Binarne(c,d) // przeniesienie poza znak
-  e <- false
+      D <- [n]
+      D[0] <- 1 // pusta tablica o 1 na koncu
+      C <- Dodaj2Binarne(C,D)) // przeniesienie poza znak
+      C <- C.tail // porzucenie najstarszego bitu ktory wyszedl poza znak
+  overFound  <- false
   if A[0] = B[0] != c[0]
-    then e <- true
-  return (c,e)
+    then overFound <- true
+  return (c,overFound)
+
 
 LinearSearch<T>(A,x:T):Option[Int] // Tablica A o dł. n i szukany element x
   s <- Nil
