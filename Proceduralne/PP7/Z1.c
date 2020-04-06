dd68bb403341edcae533330d4766333bc420e09e
BinarySoftware <mkmikolajek@icloud.com>
2020-04-06 17:59:26 +0200

    rm

--- Proceduralne/PP7/Z1.c
+++ Proceduralne/PP7/Z1.c
@@ -30,43 +30,36 @@ int main(void){
 
     while (licz < GRAN && fgets(dane[licz],ROZMIAR,stdin) != NULL && dane[licz][0] != '\0' && dane[licz][0] != '\n'){
         wsklan[licz] = &dane[licz][0];
-//        printf("%s\n", wsklan[licz]);
-//        printf("%s\n", dane[licz]);
-        //ustaw wskaźnik z tablicy wsklan na wczytany łańcuch
         licz++;
     }
+
     puts("\n Wczytano:\n");
     for (k = 1; k < licz; k++) {
         puts(*(wsklan + k));  // z użyciem identyfikataora wsklan
-//        puts(dane[k]) ;
-//        printf("wsk : %ld  tab: %ld\n", &wsklan[k][0], &dane[k][0]);
     }
 
     sortlan(wsklan, licz); // wywłoanie procedury sortującej
 
     puts("\n Oto uporzadkowana lista:\n");
     for (k = 1; k < licz; k++){
-//        puts(*(wsklan + k));  // z użyciem identyfikataora wsklan
         puts(dane[k]) ; // z użyciem identyfikataora dane
-//        printf("wsk : %ld  tab: %ld\n", &wsklan[k][0], &dane[k][0]);
     }
+
     return 0;
 }
 
 // procedura sortująca
 void sortlan(char *lan[], int num) {
+    char *temp = malloc(ROZMIAR* sizeof(char));
     //potrzebne zmienne lokalne
     for (int i = 1; i < num; i++) {
         for (int j = 1; j < num-1; j++) {
             if (strcmp(lan[j],lan[j+1]) > 0) {
-                char *temp = malloc(ROZMIAR* sizeof(char));
                 strcpy(temp,lan[j+1]);
                 strcpy(lan[j+1],lan[j]);
                 strcpy(lan[j],temp);
-//                char * temp = lan[j+1];
-//                lan[j+1]=lan[j];
-//                lan[j]=temp;
             }
         }
     }
+    free(temp);
 }
